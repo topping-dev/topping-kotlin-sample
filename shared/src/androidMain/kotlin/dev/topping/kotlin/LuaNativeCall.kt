0ed053dabe8406f716044733f8b47b8a292895ea
Deadknight <1907606+Deadknight@users.noreply.github.com>
2023-01-06 09:34:26 +0300

    *added native call for ios

--- shared/src/androidMain/kotlin/dev/topping/kotlin/LuaNativeCall.kt
+++ shared/src/androidMain/kotlin/dev/topping/kotlin/LuaNativeCall.kt
@@ -4,19 +4,19 @@ actual open class LuaNativeCall : KTInterface
 {
    var luaNativeCall: dev.topping.android.LuaNativeCall? = null
    actual companion object {
-       actual fun Call(obj: Any?, func: String?, params: ArrayList<Any?>?): LuaObjectStore?
+       actual fun Call(obj: LuaNativeObject, func: String, params: ArrayList<Any?>): LuaObjectStore?
        {
            val pobj = LuaObjectStore()
            val pres = dev.topping.android.LuaNativeCall.Call(obj, func, params)
-           pobj.SetNativeObject(pres as dev.topping.android.LuaObjectStore)
+           pobj.SetNativeObject(pres)
            return pobj;
        }
 
-       actual fun CallClass(cls: String?, func: String?, params: ArrayList<Any?>?): LuaObjectStore?
+       actual fun CallClass(cls: String, func: String, params: ArrayList<Any?>): LuaObjectStore?
        {
            val pobj = LuaObjectStore()
            val pres = dev.topping.android.LuaNativeCall.CallClass(cls, func, params)
-           pobj.SetNativeObject(pres as dev.topping.android.LuaObjectStore)
+           pobj.SetNativeObject(pres)
            return pobj
        }
    }
--- shared/src/iosArm64Main/kotlin/dev.topping.kotlin/LuaNativeCall.kt
+++ shared/src/iosArm64Main/kotlin/dev.topping.kotlin/LuaNativeCall.kt
@@ -2,33 +2,30 @@ package dev.topping.kotlin
 
 actual open class LuaNativeCall : KTInterface
 {
-   //var luaNativeCall: cocoapods.Topping.LuaNativeCall? = null
+   var luaNativeCall: cocoapods.Topping.LuaNativeCall? = null
    actual companion object {
-        actual fun Call(obj: Any?, func: String?, params: ArrayList<Any?>?): LuaObjectStore?
+        actual fun Call(obj: LuaNativeObject, func: String, params: ArrayList<Any?>): LuaObjectStore?
         {
-            /*val pobj = LuaObjectStore()
-            val pres = cocoapods.Topping.LuaNativeCall.Call(self, func, params)
-            pobj.SetNativeObject(pres as cocoapods.Topping.LuaObjectStore)
-            return pobj*/
-            return null
+            val pobj = LuaObjectStore()
+            val pres = cocoapods.Topping.LuaNativeCall.Call(obj.luaNativeObject!!, func, params)
+            pobj.SetNativeObject(pres)
+            return pobj
         }
 
-        actual fun CallClass(cls: String?, func: String?, params: ArrayList<Any?>?): LuaObjectStore?
+        actual fun CallClass(cls: String, func: String, params: ArrayList<Any?>): LuaObjectStore?
         {
-           /*val pobj = LuaObjectStore()
-           val pres = cocoapods.Topping.LuaNativeCall.Call(self, func, params)
-           pobj.SetNativeObject(pres as cocoapods.Topping.LuaObjectStore)
-           return pobj*/
-           return null
+           val pobj = LuaObjectStore()
+           val pres = cocoapods.Topping.LuaNativeCall.CallClass(cls, func, params)
+           pobj.SetNativeObject(pres)
+           return pobj
         }
    }
     open override fun GetNativeObject(): Any?
    {
-       //return luaNativeCall
-       return null
+       return luaNativeCall
    }
     open override fun SetNativeObject(par :Any?)
    {
-       //luaNativeCall = par as LuaNativeCall?
+       luaNativeCall = par as cocoapods.Topping.LuaNativeCall?
    }
 }
